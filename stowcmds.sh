1e214197913feeb08a60569b8d16a634bad8be6c
Raymaekers Luca <luca@keyfried.com>
2023-02-20 20:02:56 +0100

    added: (stowcmds) laptop stowcmds and error handling

--- stowcmds.sh
+++ stowcmds.sh
@@ -1,7 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-case "${MACH:=desktop}" in
-	'desktop')
+if ! stow --version > /dev/null 2>&1
+then
+	echo "E: stow not installed or not found" 1>&2
+	exit 1
+fi
+if [ -n "$MACH" ]
+then
+	echo "I: stowing for $MACH"
+else
+	echo "E: MACH not set"
+	exit 1
+fi
+
+case "$MACH" in
+	"desktop")
 		mkdir -p "$HOME/bin"
 		stow -d bin/ -t "$HOME/bin" -R common dmscripts extra
 		mkdir -p "$HOME/.config"
@@ -13,6 +26,13 @@ case "${MACH:=desktop}" in
 		stow -d bin/ -t "$HOME/bin" -R common
 		mkdir -p "$HOME/.config"
 		stow -d config/ -t "$HOME/.config" -R essentials common
+		stow -d config/ -t "$HOME/" -R zshrc
+		;;
+	"laptop")
+		mkdir -p "$HOME/bin"
+		stow -d bin/ -t "$HOME/bin" -R common dmscripts extra
+		mkdir -p "$HOME/.config"
+		stow -d config/ -t "$HOME/.config" -R essentials common extra theme hyprland X
 		stow -d config/ -t "$HOME/" -R zshrc
 		;;
 	*)
