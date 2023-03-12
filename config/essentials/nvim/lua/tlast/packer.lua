7d37aa6310fd9ba4ae545b14d925287d5f2228d4
Raymaekers Luca <luca@keyfried.com>
2023-03-12 15:10:03 +0100

    readded telescope-symbols

--- config/essentials/nvim/lua/tlast/packer.lua
+++ config/essentials/nvim/lua/tlast/packer.lua
@@ -6,6 +6,7 @@ return require('packer').startup(function(use)
 		'nvim-telescope/telescope.nvim', tag = '0.1.1',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
+	use('nvim-telescope/telescope-symbols.nvim')
 	use('theprimeagen/harpoon')
 
 	-- colors
