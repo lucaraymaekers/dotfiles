5ac957d217d246b80cbf4d5203ae759b315190d3
Raymaekers Luca <luca@keyfried.com>
2023-05-04 10:42:48 +0200

    use date variable for filename

--- config/hyprland/hypr/screenshot.sh
+++ config/hyprland/hypr/screenshot.sh
@@ -2,6 +2,7 @@
 
 dir="$(xdg-user-dir PICTURES)"
 dir="${dir:-$HOME/pics}/screenshots"
+date="$(date +%y%m%d_%H_%M_%S)"
 mkdir -p "$dir"
 
 case $1 in
@@ -10,13 +11,13 @@ case $1 in
 		actmon="$(hyprctl monitors -j |
 			jq -r ".[] | select(.id == $actwin)" |
 			jq -r '.name')" 
-		grim -o "$actmon" "$dir/$(date +%y%m%d_%H_%M_%S)_mon.png"
+		grim -o "$actmon" "$dir/${date}_mon.png"
 		;;
 	"-f")
-		grim "$dir/$(date +%y%m%d_%H_%M_%S)_full.png"
+		grim "$dir/${date}_full.png"
 		;;
 	"-s")
-		grim -g "$(slurp)" "$dir/$(date +%y%m%d_%H_%M_%S)_sel.png"
+		grim -g "$(slurp)" "$dir/${date}_sel.png"
 		;;
 	"-sc")
 		grim -g "$(slurp)" - | wl-copy
