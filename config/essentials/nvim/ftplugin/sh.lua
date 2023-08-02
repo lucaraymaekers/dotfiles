bd8efd2dd7ae3d24c4b5f8a01640082feaaa8b34
Raymaekers Luca <luca@keyfried.com>
2023-08-02 10:43:39 +0200

    added variable keymap to sh

--- config/essentials/nvim/ftplugin/sh.lua
+++ config/essentials/nvim/ftplugin/sh.lua
@@ -2,5 +2,5 @@ vim.keymap.set("n", "<LocalLeader>t", "<cmd>s/\\(\\s*\\)\\(.*\\)/\\1echo \"\\2\"
 vim.keymap.set("n", "<LocalLeader>i", "<cmd>s/\\(\\s*\\)\\(.*\\)/\\1echo \"\\2: $\\2\"<cr>")
 vim.keymap.set("i", "<LocalLeader>t", "<cmd>s/\\(\\s*\\)\\(.*\\)/\\1echo \"\\2\"<cr><esc>A")
 vim.keymap.set("i", "<LocalLeader>i", "<cmd>s/\\(\\s*\\)\\(.*\\)/\\1echo \"\\2: $\\2\"<cr><esc>A")
-vim.keymap.set({"i", "n"}, "<LocalLeader>v", "<esc>A)\"<esc>I\"$(<esc>0")
+vim.keymap.set({"i", "n"}, "<LocalLeader>v", "<esc>A)\"<esc>I\"$(<esc>I")
 vim.opt.formatoptions = "cqrnj"
