033cef806d00b1c4b0cf4e9eaefa7ebff9d6619c
Raymaekers Luca <luca@keyfried.com>
2023-09-22 13:06:07 +0200

    [nvim] added neotree plugin

--- config/essentials/nvim/lua/user/packer.lua
+++ config/essentials/nvim/lua/user/packer.lua
@@ -25,6 +25,15 @@ return require('packer').startup(function(use)
 	use('theRealCarneiro/hyprland-vim-syntax')
 
 	use('mbbill/undotree')
+	use {
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		}
+	}
 
 	use('tpope/vim-capslock')
 	use('tpope/vim-commentary')
