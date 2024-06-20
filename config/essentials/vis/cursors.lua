eaccecd53978ae4d93cb34ecc354068536609c96
Raymaekers Luca <luca@keyfried.com>
2024-06-20 22:50:03 +0200

    checkpoint

--- config/essentials/vis/cursors.lua
+++ config/essentials/vis/cursors.lua
@@ -7,10 +7,10 @@ M.maxsize = 1000
 
 -- get the default system cache directory
 local get_default_cache_path = function()
-	local HOME = os.getenv('HOME')
-	local XDG_CACHE_HOME = os.getenv('XDG_CACHE_HOME')
-	local BASE = XDG_CACHE_HOME or HOME
-	return BASE .. '/.vis-cursors'
+    local HOME = os.getenv('HOME')
+    local XDG_CACHE_HOME = os.getenv('XDG_CACHE_HOME')
+    local BASE = XDG_CACHE_HOME or HOME
+    return BASE .. '/.vis-cursors'
 end
 
 -- default save path
@@ -18,100 +18,83 @@ M.path = get_default_cache_path()
 
 local read_files = function()
 
-	-- read file
-	local file = io.open(M.path)
-	if file == nil then
-		return
-	end
+    -- read file
+    local file = io.open(M.path)
+    if file == nil then return end
 
-	files = {}
+    files = {}
 
-	-- read positions per file path
-	for line in file:lines() do
-		for path, pos in string.gmatch(line, '(.+)[,%s](%d+)') do
-			cursors[path] = pos
-			table.insert(files, path)
-		end
-	end
+    -- read positions per file path
+    for line in file:lines() do
+        for path, pos in string.gmatch(line, '(.+)[,%s](%d+)') do
+            cursors[path] = pos
+            table.insert(files, path)
+        end
+    end
 
-	file:close()
+    file:close()
 end
 
 -- read cursors from file on init
-local on_init = function()
-	read_files()
-end
+local on_init = function() read_files() end
 
 -- apply cursor pos on win open
 local on_win_open = function(win)
 
-	if win.file == nil or win.file.path == nil then
-		return
-	end
+    if win.file == nil or win.file.path == nil then return end
 
-	-- init cursor path if nil
-	local pos = cursors[win.file.path]
-	if pos == nil then
-		cursors[win.file.path] = win.selection.pos
-		return
-	end
+    -- init cursor path if nil
+    local pos = cursors[win.file.path]
+    if pos == nil then
+        cursors[win.file.path] = win.selection.pos
+        return
+    end
 
-	-- set current cursor
-	win.selection.pos = tonumber(pos)
+    -- set current cursor
+    win.selection.pos = tonumber(pos)
 
-	-- center view around cursor
-	vis:feedkeys("zz")
+    -- center view around cursor
+    vis:feedkeys("zz")
 end
 
 -- set cursor pos on close
 local on_win_close = function(win)
 
-	if win.file == nil or win.file.path == nil then
-		return
-	end
+    if win.file == nil or win.file.path == nil then return end
 
-	-- re-read files in case they've changed
-	read_files()
+    -- re-read files in case they've changed
+    read_files()
 
-	-- remove old occurences of current path
-	for i, path in ipairs(files) do
-		if path == win.file.path then
-			table.remove(files, i)
-		end
-	end
+    -- remove old occurences of current path
+    for i, path in ipairs(files) do
+        if path == win.file.path then table.remove(files, i) end
+    end
 
-	-- ignore files with cursor at the beginning
-	if win.selection.pos == 0 then
-		return
-	end
+    -- ignore files with cursor at the beginning
+    if win.selection.pos == 0 then return end
 
+    -- insert current path to top of files
+    table.insert(files, 1, win.file.path)
 
-	-- insert current path to top of files
-	table.insert(files, 1, win.file.path)
-
-	-- set cursor pos for current file path
-	cursors[win.file.path] = win.selection.pos
+    -- set cursor pos for current file path
+    cursors[win.file.path] = win.selection.pos
 end
 
 -- write cursors to file on quit
 local on_quit = function()
 
-	local file = io.open(M.path, 'w+')
-	if file == nil then
-		return
-	end
+    local file = io.open(M.path, 'w+')
+    if file == nil then return end
 
-	-- buffer cursors string
-	local buffer = {}
-	for _, path in ipairs(files) do
-		table.insert(buffer, string.format('%s,%d', path, cursors[path]))
-		if M.maxsize and #buffer >= M.maxsize then
-			break
-		end
-	end
-	local output = table.concat(buffer, '\n')
-	file:write(output)
-	file:close()
+    -- buffer cursors string
+    local buffer = {}
+    for _, path in ipairs(files) do
+        table.insert(buffer, string.format('%s,%d', path, cursors[path]))
+        if M.maxsize and #buffer >= M.maxsize then break end
+    end
+    local output = table.concat(buffer, '\n')
+    file:write(output)
+    file:close()
 end
 
 vis.events.subscribe(vis.events.INIT, on_init)
--- config/essentials/vis/visrc.lua
+++ config/essentials/vis/visrc.lua
@@ -1,41 +1,38 @@
 ------------------------------------
 --- LIBRARIES
 ------------------------------------
-
 require('vis')
 
 -- plugins
+require("build")
 require("backup")
 require("cursors")
 require("title")
 
+vis:command_register("make", function() vis:communicate() end, "make")
 
 ------------------------------------
 --- FUNCTIONS
 ------------------------------------
 
 local map_cmd = function(mode, map, command, help)
-	vis:map(mode, map, function() vis:command(command) end, help)
+    vis:map(mode, map, function() vis:command(command) end, help)
 end
 
 local map_cmd_restore = function(mode, map, command, help)
-	vis:map(mode, map, function()
-		if (mode == vis.modes.INSERT) then
-			vis:feedkeys("<Escape>")
-		end
+    vis:map(mode, map, function()
+        if (mode == vis.modes.INSERT) then vis:feedkeys("<Escape>") end
 
-		vis:feedkeys("m")
-		vis:command(command)
-		vis:feedkeys("M")
+        vis:feedkeys("m")
+        vis:command(command)
+        vis:feedkeys("M")
 
-		if (mode == vis.modes.INSERT) then
-			vis:feedkeys("i")
-		end
-	end, help)
+        if (mode == vis.modes.INSERT) then vis:feedkeys("i") end
+    end, help)
 end
 
 local map_keys = function(mode, map, keys, help)
-	vis:map(mode, map, function() vis:feedkeys(keys) end, help)
+    vis:map(mode, map, function() vis:feedkeys(keys) end, help)
 end
 
 ------------------------------------
@@ -49,6 +46,9 @@ local m = vis.modes
 -----------------------------------
 
 vis:command_register("Q", function() vis:command("qa!") end, "Quit all")
+vis:command_register("delws",
+                     function() vis:command("x/[ \t]+$|^[ \t]+$/d") end,
+                     "Remove trailing whitespace")
 
 -------------------------------------
 --- MAPPINGS
@@ -60,39 +60,37 @@ map_cmd(m.NORMAL, " c", "e ~/.config/vis/visrc.lua", "Edit config file")
 map_cmd(m.NORMAL, " q", "q!", "Quit (force)")
 map_cmd(m.NORMAL, " s", "!doas vis $vis_filepath", "Edit as superuser")
 map_cmd(m.NORMAL, " w", "w", "Write")
-map_cmd(m.NORMAL, " x", "!chmod u+x $vis_filepath", "Make active file executable")
+map_cmd(m.NORMAL, " x", "!chmod u+x $vis_filepath",
+        "Make active file executable")
 
 vis:map(m.NORMAL, " eh", function()
-	vis:command("!lowdown $vis_filepath > ${vis_filepath%.md}.html")
-	vis:info("exported.")
+    vis:command("!lowdown $vis_filepath > ${vis_filepath%.md}.html")
+    vis:info("exported.")
 end, "Export markdown to html")
 
 map_keys(m.NORMAL, " nl", ":<seq -f '%0.0f. ' 1 ", "Insert numbered list")
-map_keys(m.NORMAL, " ws", ":,x/[ \t]+$|^[ \t]+$/d<Enter>", "Remove trailing whitespace")
 
 -- select markdown list element:	,x/^(\d+\.|[-*])\s+.+\n(^ .+\n)*/
-
-
 
 ------------------------------------
 --- EVENTS
 ------------------------------------
 
 vis.events.subscribe(vis.events.INIT, function()
-	vis.options.ignorecase = true
-	vis.options.autoindent = true
-	vis.options.shell = "/bin/sh"
-	local theme = "nord"
-	vis:command("set theme " .. theme)
+    vis.options.ignorecase = true
+    vis.options.autoindent = true
+    vis.options.shell = "/bin/sh"
+    local theme = "nord"
+    vis:command("set theme " .. theme)
 end)
 
-vis.events.subscribe(vis.events.WIN_OPEN, function(win) -- luacheck: no unused args
-	win.options.relativenumbers = true
+vis.events.subscribe(vis.events.WIN_OPEN,
+                     function(win) -- luacheck: no unused args
+    win.options.relativenumbers = true
 
-	if win.syntax == "bash" then
-		map_keys(m.NORMAL, " v",
-		"V:x/^(\\s*)(.+)$/ c/\\1>\\&2 printf '\\2: %s\\\\n' \"$\\2\"/<Enter><Escape>", "Print variable")
-	end
-
+    if win.syntax == "bash" then
+        map_keys(m.NORMAL, " v",
+                 "V:x/^(\\s*)(.+)$/ c/\\1>\\&2 printf '\\2: %s\\\\n' \"$\\2\"/<Enter><Escape>",
+                 "Print variable")
+    end
 end)
-
